0fd9dca511ddaf5a965549aab6df30a60f2e1c17
PENDA HANS <60019886+PENDAHANS@users.noreply.github.com>
2023-10-20 02:14:46 +0100

    Add files via upload

---
+++ aes-algorithm.py
@@ -0,0 +1,45 @@
+import hashlib, sys, os
+from Crypto.Cipher import AES
+from Crypto.Util import Padding
+
+def encryptFile(password, IV, filename):
+    key = hashlib.sha256(password.encode()).digest()
+
+    with open(filename, 'rb') as f:
+        file_data = f.read()
+    
+    cipher = AES.new(key, AES.MODE_CBC,IV)
+    padded_file_data = Padding.pad(file_data,16)
+    encrypted_data = cipher.encrypt(padded_file_data)
+
+    with open(filename, 'wb') as f:
+        f.write(encrypted_data)
+    return 'operation effectuer'
+
+def decryptFile(password, IV, filename):
+    key = hashlib.sha256(password.encode()).digest()
+
+    with open(filename, 'rb') as ef:
+        en_file_data = ef.read()
+    
+    cipher = AES.new(key, AES.MODE_CBC,IV)
+    decrypted_data = cipher.decrypt(en_file_data)
+    original_file_data = Padding.unpad(decrypted_data,16)
+
+    with open(filename, 'wb') as f:
+        f.write(original_file_data)
+    return 'operation effectuer'
+repertoire=("cd C:/Users/HANS PENDA/Downloads/AES/")
+#if sys.argv[1] == 'crypter':
+#    print(encryptFile(sys.argv[2],b'P0123as235a145df',sys.argv[3]))
+#elif sys.argv[1] == 'decrypter':
+#    print(decryptFile(sys.argv[2],b'P0123as235a145df',sys.argv[3]))
+if sys.argv[1] == 'crypter_dossier':
+   print(encryptFile(sys.argv[2],b'P0123as235a145df',sys.argv[3]))
+   #os.system('cmd /k'+repertoire+'' and 'cmd /k "cipher /e"' )
+   print('operation effectuer')
+elif sys.argv[1] == 'decrypter_dossier':
+    print(decryptFile(sys.argv[2],b'P0123as235a145df',sys.argv[3]))
+   #os.system('cmd /k'+repertoire+'' and 'cmd /k "cipher /d"' )
+else:
+    print('mauvais choix!')
